5757f55037bdaecf04b995f71b130abea46f64d5
Minki Kim <mingi3314@gmail.com>
2024-06-30 23:54:05 +0900

    ✨ add init command

--- tradelens/main.py
+++ tradelens/main.py
@@ -1,16 +1,70 @@
+import json
 import os
+from pathlib import Path
 
 import typer
 from jinja2 import Template
 
 from tradelens.models import TradeLog
 from tradelens.parsers import TradeLogParserFactory, TradeLogParserType
+from tradelens.settings import Settings
 from tradelens.utils import load_template
 
 app = typer.Typer()
 
+config_path = Path(typer.get_app_dir("tradelens")) / "config.json"
 
-def write_journal(trade_log: TradeLog, template: Template, output_dir: str) -> None:
+
+@app.command()
+def init() -> None:
+    """
+    Initialize the configuration for trade journal.
+    """
+    output_dir = typer.prompt(text="Enter the directory to save journals", type=Path)
+
+    config = {"output_dir": output_dir}
+
+    config_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(config_path, "w") as f:
+        json.dump(config, f)
+
+    typer.secho("Configuration saved successfully.", fg=typer.colors.GREEN)
+
+
+@app.command()
+def generate_notes(
+    output_dir: str = "notes", template_path: str = "tradelens/templates/journal_template.md"
+) -> None:
+    """
+    Generate Obsidian notes from clipboard data.
+    """
+    config = _load_config()
+    output_dir = config.output_dir
+
+    typer.echo(f"Generating notes in {output_dir} using template {template_path}")
+    try:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # 템플릿 로드
+        template = load_template(template_path)
+
+        # 파서 인스턴스 생성
+        parser = TradeLogParserFactory.create_parser(TradeLogParserType.KIWOOM_HTS_CLIPBOARD)
+        trade_logs = parser.parse()
+
+        # 각 row에 대해 노트를 생성
+        for trade_log in trade_logs:
+            _write_journal(trade_log, template, output_dir)
+
+        typer.secho("Notes generated successfully!", fg=typer.colors.GREEN)
+
+    except Exception as e:
+        typer.secho(f"Error reading clipboard data or generating notes:\n{e}", fg=typer.colors.RED)
+
+
+def _write_journal(trade_log: TradeLog, template: Template, output_dir: str) -> None:
     try:
         note_content = template.render(
             date=trade_log.date,
@@ -37,32 +91,17 @@ def write_journal(trade_log: TradeLog, template: Template, output_dir: str) -> N
         )
 
 
-@app.command()
-def generate_notes(
-    output_dir: str = "notes", template_path: str = "tradelens/templates/journal_template.md"
-) -> None:
-    """
-    Generate Obsidian notes from clipboard data.
-    """
-    try:
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+def _load_config() -> Settings:
+    if not config_path.exists():
+        typer.secho(
+            "Configuration file not found. Please run 'init' command first.", fg=typer.colors.RED
+        )
+        raise typer.Exit()
 
-        # 템플릿 로드
-        template = load_template(template_path)
+    with open(config_path) as f:
+        config_data = json.load(f)
 
-        # 파서 인스턴스 생성
-        parser = TradeLogParserFactory.create_parser(TradeLogParserType.KIWOOM_HTS_CLIPBOARD)
-        trade_logs = parser.parse()
-
-        # 각 row에 대해 노트를 생성
-        for trade_log in trade_logs:
-            write_journal(trade_log, template, output_dir)
-
-        typer.secho("Notes generated successfully!", fg=typer.colors.GREEN)
-
-    except Exception as e:
-        typer.secho(f"Error reading clipboard data or generating notes:\n{e}", fg=typer.colors.RED)
+    return Settings(**config_data)
 
 
 if __name__ == "__main__":
